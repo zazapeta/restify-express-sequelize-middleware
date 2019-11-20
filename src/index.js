4b1805ddeada553fe0a3c8f7ea063a7d60a1504c
zazapeta <sami.ghazouane@gmail.com>
2019-11-20 14:37:56 +0100

    Extracted some models utils. Added modelsSelector, validateModelSelector, pathModelSelector to avoid duplication

--- src/index.js
+++ src/index.js
@@ -2,81 +2,89 @@ const express = require("express");
 const Joi = require("@hapi/joi");
 const Boom = require("@hapi/boom");
 
+const {
+  modelsSelector,
+  validateModelSelector,
+  pathModelSelector
+} = require("./utils");
+
 function boomIt(res, b) {
   res.status(b.output.statusCode).json(b.output.payload);
 }
 
 module.exports = ({ app, sequelize }) => {
   app.use(express.json());
-  Object.values(sequelize.models)
-    .filter(model => model.name !== "SequelizeMeta")
-    .forEach(model => {
-      const path = model.options.name.plural.toLowerCase();
-      const {
-        validate: { create, readOne, readlAll, update, delete: deletion }
-      } = model.restify;
-      /**
-       * CREATE
-       */
-      app.post(`/${path}`, async (req, res, next) => {
-        let resourceData = req.body;
-        let boom;
-        if (create && typeof create === "object") {
-          const { error, value } = Joi.object(create).validate(resourceData);
-          if (error) {
-            boom = Boom.badRequest(error);
-          } else {
-            resourceData = value;
-          }
-        } else if (create && typeof create === "function") {
-          // create validator should return a object { error, value }
-          const { error, value } = create(req, res, next);
-          if (error) {
-            boom = Boom.badRequest(error);
-          } else {
-            resourceData = value;
-          }
-        }
-        if (boom) {
-          boomIt(res, boom);
+  modelsSelector(sequelize).forEach(model => {
+    const path = pathModelSelector(model);
+    const {
+      create,
+      readOne,
+      readlAll,
+      update,
+      delete: deletion
+    } = validateModelSelector(model);
+    /**
+     * CREATE
+     */
+    app.post(`/${path}`, async (req, res, next) => {
+      let resourceData = req.body;
+      let boom;
+      if (create && typeof create === "object") {
+        const { error, value } = Joi.object(create).validate(resourceData);
+        if (error) {
+          boom = Boom.badRequest(error);
         } else {
-          const createdResource = await model.create(resourceData);
-          res.status(201).json(createdResource);
+          resourceData = value;
         }
-      });
+      } else if (create && typeof create === "function") {
+        // create validator should return a object { error, value }
+        const { error, value } = create(req, res, next);
+        if (error) {
+          boom = Boom.badRequest(error);
+        } else {
+          resourceData = value;
+        }
+      }
+      if (boom) {
+        boomIt(res, boom);
+      } else {
+        const createdResource = await model.create(resourceData);
+        res.status(201).json(createdResource);
+      }
+    });
 
-      /**
-       * READ
-       */
-      // READ ONE
-      app.get(`/${path}/:id`, (req, res, next) => {
-        model.findByPk(req.params.id).then(resource => res.json(resource));
-      });
-      // READ ALL
-      app.get(`/${path}`, (req, res, next) => {
-        model.findAll().then(resources => {
-          res.json(resources);
-        });
-      });
-
-      /**
-       * UPDATE
-       */
-      app.put(`/${path}/:id`, async (req, res, next) => {
-        let resource = await model.findByPk(req.params.id);
-        await resource.update(req.body);
-        res.status(200).json(resource);
-      });
-
-      /**
-       * DELETE
-       */
-      app.delete(`/${path}/:id`, async (req, res, next) => {
-        let resource = await model.findByPk(req.params.id);
-        await resource.destroy();
-        res.json(resource);
+    /**
+     * READ
+     */
+    // READ ONE
+    app.get(`/${path}/:id`, (req, res, next) => {
+      model.findByPk(req.params.id).then(resource => res.json(resource));
+    });
+    // READ ALL
+    app.get(`/${path}`, (req, res, next) => {
+      model.findAll().then(resources => {
+        res.json(resources);
       });
     });
+
+    /**
+     * UPDATE
+     */
+    app.put(`/${path}/:id`, async (req, res, next) => {
+      let resource = await model.findByPk(req.params.id);
+      await resource.update(req.body);
+      res.status(200).json(resource);
+    });
+
+    /**
+     * DELETE
+     */
+    app.delete(`/${path}/:id`, async (req, res, next) => {
+      let resource = await model.findByPk(req.params.id);
+      await resource.destroy();
+      res.json(resource);
+    });
+  });
   return app;
   // (req, res, next) => {
   //   if (model) {
---
+++ src/utils.js
@@ -0,0 +1,39 @@
+/**
+ * TODO: test it
+ * @param {Object} sequelize
+ * @returns {Array<sequelize.Model>} list of models
+ */
+function modelsSelector({ models }) {
+  return Object.values(models).filter(model => model.name !== "SequelizeMeta");
+}
+
+/**
+ * TODO: test it
+ * @param {Object} sequelizeModel
+ * @returns {Object} an object within {
+      create,
+      readOne,
+      readlAll,
+      update,
+      delete
+    }
+ */
+function validateModelSelector({ restify }) {
+  return restify.validate;
+}
+
+/**
+ * TODO: test it
+ * return path name from a model
+ * @param {Object} model
+ * @returns {String} the path name
+ */
+function pathModelSelector(model) {
+  return model.options.name.plural.toLowerCase();
+}
+
+module.exports = {
+  modelsSelector,
+  validateModelSelector,
+  pathModelSelector
+};
